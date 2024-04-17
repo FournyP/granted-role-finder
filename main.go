6b11d7d6824109403a00a5c494f98a450c9e2e36
FournyP <pierre.fourny76@gmail.com>
2024-04-17 20:27:03 +0200

    Finish to implement granted-role-finder

--- main.go
+++ main.go
@@ -1,38 +1,32 @@
 package main
 
 import (
-	"context"
 	"flag"
+	"fmt"
 	"log"
 	"strings"
 	"time"
 
-	"github.com/FournyP/granted-role-finder/abis"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/nanmu42/etherscan-api"
-	"github.com/samber/lo"
-	lop "github.com/samber/lo/parallel"
+)
+
+const (
+	ROLE_GRANTED_TOPIC = "0x2f8788117e7eff1d82e926ec794901d17c78024a50270940304540a733656f0d"
+	ROLE_REVOKED_TOPIC = "0xf6391f5c32d9c69d2a47ea670b442974b53935d1edc7fd64eb21e047a839171b"
 )
 
 func main() {
-	var rpcUrl, etherscanApiKey, etherscanBaseUrl, smartContractAddress, topic string
+	var etherscanApiKey, etherscanBaseUrl, smartContractAddress string
 	var fromBlock, toBlock int64
 
-	flag.StringVar(&rpcUrl, "rpc-url", "", "RPC URL of the Ethereum node.")
 	flag.StringVar(&etherscanApiKey, "etherscan-api-key", "", "Etherscan API key.")
 	flag.StringVar(&etherscanBaseUrl, "etherscan-base-url", "", "Etherscan base URL.")
 	flag.StringVar(&smartContractAddress, "sc-address", "", "Smart contract address to scan.")
-	flag.StringVar(&topic, "topic", "0x2f8788117e7eff1d82e926ec794901d17c78024a50270940304540a733656f0d", "Topic to filter logs.")
 
 	flag.Int64Var(&toBlock, "to-block", 0, "To block number.")
 	flag.Int64Var(&fromBlock, "from-block", 0, "From block number.")
 
 	flag.Parse()
-
-	if len(strings.TrimSpace(rpcUrl)) == 0 {
-		panic("RPC URL is required.")
-	}
 
 	if len(strings.TrimSpace(etherscanApiKey)) == 0 {
 		panic("Etherscan API key is required.")
@@ -46,13 +40,12 @@ func main() {
 		panic("Smart contract address is required.")
 	}
 
-	if len(strings.TrimSpace(topic)) == 0 {
-		panic("Topic is required.")
+	if fromBlock == 0 {
+		panic("From block number is required.")
 	}
 
-	ethClient, err := ethclient.Dial(rpcUrl)
-	if err != nil {
-		log.Fatal(err)
+	if toBlock == 0 {
+		panic("To block number is required.")
 	}
 
 	etherscanClient := etherscan.NewCustomized(etherscan.Customization{
@@ -62,46 +55,41 @@ func main() {
 		Verbose: false,
 	})
 
-	if toBlock == 0 {
-		header, err := ethClient.HeaderByNumber(context.Background(), nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		toBlock = header.Number.Int64()
-	}
-
-	ethLogs, err := etherscanClient.GetLogs(int(fromBlock), int(toBlock), smartContractAddress, topic)
+	roleGrantedEthLogs, err := etherscanClient.GetLogs(int(fromBlock), int(toBlock), smartContractAddress, ROLE_GRANTED_TOPIC)
 	if err != nil {
 		log.Fatal("Unable to find logs :", err)
 	}
 
-	hashs := lop.Map(ethLogs, func(value etherscan.Log, _ int) string {
-		return value.TransactionHash
-	})
+	log.Println("Role granted logs:")
 
-	hashs = lo.Uniq(hashs)
+	for _, ethLog := range roleGrantedEthLogs {
+		role := ethLog.Topics[1]
+		account := fmt.Sprintf("0x%s", ethLog.Topics[2][26:])
+		sender := fmt.Sprintf("0x%s", ethLog.Topics[3][26:])
 
-	for _, hash := range hashs {
-		txHash := common.HexToHash(hash)
-		receipt, err := ethClient.TransactionReceipt(context.Background(), txHash)
-		if err != nil {
-			log.Fatal("Unable to get transaction receipt : ", hash, err)
-		}
+		log.Println("-----------------------")
+		log.Println("Role:", role)
+		log.Println("Account:", account)
+		log.Println("Sender:", sender)
+		log.Println("-----------------------")
+	}
 
-		for _, ethLog := range receipt.Logs {
-			accessControl, err := abis.NewAccessControl(ethLog.Address, ethClient)
-			if err != nil {
-				log.Fatal("Unable to get AccessControl contract : ", err)
-			}
+	roleRevokedEthLogs, err := etherscanClient.GetLogs(int(fromBlock), int(toBlock), smartContractAddress, ROLE_REVOKED_TOPIC)
+	if err != nil {
+		log.Fatal("Unable to find logs :", err)
+	}
 
-			event, err := accessControl.ParseRoleGranted(*ethLog)
-			if err != nil {
-				log.Printf("Failed to unpack log: %v", err)
-				continue
-			}
+	log.Println("Role revoked logs:")
 
-			log.Printf("Role: %s", event)
-		}
+	for _, ethLog := range roleRevokedEthLogs {
+		role := ethLog.Topics[1]
+		account := fmt.Sprintf("0x%s", ethLog.Topics[2][26:])
+		sender := fmt.Sprintf("0x%s", ethLog.Topics[3][26:])
+
+		log.Println("-----------------------")
+		log.Println("Role:", role)
+		log.Println("Account:", account)
+		log.Println("Sender:", sender)
+		log.Println("-----------------------")
 	}
 }
